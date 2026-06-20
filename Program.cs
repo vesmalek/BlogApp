3bad05b13bdde90abf8478150b70cf4a9827cf01
Ismail Malik <vesmalick@gmail.com>
2026-06-21 02:25:36 +0300

    setup database connection

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 using BlogApp.Components;
+using BlogApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
